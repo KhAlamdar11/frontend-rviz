541748fd110bb2743222ec92b1d85d1032af1502
KhAlamdar11 <75181406+KhAlamdar11@users.noreply.github.com>
2022-04-24 14:21:29 +0500

    Update debugging_frontend.py

--- src/debugging_frontend.py
+++ src/debugging_frontend.py
@@ -36,6 +36,7 @@ class visualize_topics:
         self.myMarker = Marker()
         self.myMarker.header.frame_id = "/map"
         self.myMarker.header.stamp  = rospy.get_rostime()
+        self.motion_busy = None
         #self.myMarker.ns = "window"
         self.myMarker.id = 1
         self.myMarker.type = self.myMarker.SPHERE # sphere
@@ -69,6 +70,8 @@ class visualize_topics:
 
         self.waypoints_sub = rospy.Subscriber("/frontier/waypoints",Float64MultiArray,self.path_pub) #occupancy grid publisher
         self.human_loc_sub = rospy.Subscriber("human_detection/human_locations",Float64MultiArray,self.human_pub)
+
+        self.motion_planner_busy = rospy.Subscriber("/motion_planner/motion_busy",Int8,self.motion_busy_update) #occupancy grid publisher
         print("Hello Im here")
         #self.path_pub([3,1,1,1,2,2,2, 3,3,3])
         #self.frontier_status_publisher = rospy.Publisher("/frontier/status",Int32MultiArray,queue_size=1) #occupancy grid publisher
@@ -82,6 +85,8 @@ class visualize_topics:
 
         
         #self.waypoints_sub = rospy.Subscriber("/frontier/waypoints",Float64MultiArray,queue_size=10) #occupancy grid publisher
+    def motion_busy_update(self,data):
+        self.motion_busy = data.data
 
     def path_pub(self,data):   
         data = data.data
@@ -89,38 +94,39 @@ class visualize_topics:
         # while not rospy.is_shutdown():
         self.marker_Arr.markers = []
         for i in range(1,len(data),3):
-            self.myMarker = Marker()
-            self.myMarker.header.frame_id = "/map"
-            self.myMarker.type = self.myMarker.SPHERE # sphere
-            self.myMarker.action = self.myMarker.ADD
+            if self.motion_busy==0:
+                self.myMarker = Marker()
+                self.myMarker.header.frame_id = "/map"
+                self.myMarker.type = self.myMarker.SPHERE # sphere
+                self.myMarker.action = self.myMarker.ADD
 
-            self.myPoint = Point()
-            self.myPoint.x = data[i+1]
-            self.myPoint.y = -data[i]
-            print(self.myPoint.x,self.myPoint.y)
-            self.myMarker.pose.position = self.myPoint
+                self.myPoint = Point()
+                self.myPoint.x = data[i+1]
+                self.myPoint.y = -data[i]
+                print(self.myPoint.x,self.myPoint.y)
+                self.myMarker.pose.position = self.myPoint
+                
+                q = quaternion_from_euler(0, 0, 0)
+                self.myMarker.pose.orientation.x=q[0]
+                self.myMarker.pose.orientation.y=q[1]
+                self.myMarker.pose.orientation.z=q[2]
+                self.myMarker.pose.orientation.w=q[3]
+                self.myMarker.color=ColorRGBA(0, 1, 0, 1)
+                self.myMarker.scale.x = 0.2
+                self.myMarker.scale.y = 0.2
+                self.myMarker.scale.z = 0.2
+                self.myMarker.lifetime = rospy.Duration(0)
+                
+                self.marker_Arr.markers.append(self.myMarker)
+                id = 0
+                for m in self.marker_Arr.markers:
+                    m.id = id
+                    id += 1
+                self.path_publisher.publish(self.marker_Arr)
+                print("Im inside")
+            else:
+                print("Currently Busy Executing Path")
             
-            q = quaternion_from_euler(0, 0, 0)
-            self.myMarker.pose.orientation.x=q[0]
-            self.myMarker.pose.orientation.y=q[1]
-            self.myMarker.pose.orientation.z=q[2]
-            self.myMarker.pose.orientation.w=q[3]
-            self.myMarker.color=ColorRGBA(0, 1, 0, 1)
-            self.myMarker.scale.x = 0.2
-            self.myMarker.scale.y = 0.2
-            self.myMarker.scale.z = 0.2
-            self.myMarker.lifetime = rospy.Duration(0)
-            
-            self.marker_Arr.markers.append(self.myMarker)
-        id = 0
-        for m in self.marker_Arr.markers:
-            m.id = id
-            id += 1
-
-        #self.first_publisher.publish(self.myMarker)
-        self.path_publisher.publish(self.marker_Arr)
-        print("Im inside")
-        #        self.rate.sleep()
 
     def human_pub(self,data):
         human_loc_arr = data.data
